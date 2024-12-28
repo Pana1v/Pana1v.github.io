4af787f509182464b48945f9653a47f47c8446da
Panav <63401208+Pana1v@users.noreply.github.com>
2024-12-29 05:10:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/pana1v.github.io/', // Replace 'your-repo-name' with your GitHub repository name
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ['lucide-react'], // Exclude 'lucide-react' if necessary
   },
 });
